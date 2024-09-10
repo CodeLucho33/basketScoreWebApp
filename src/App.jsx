2b7f80098a5008c4e3539fc4e817002e317058ea
CodeLucho33 <code.lucho33@gmail.com>
2024-09-10 09:31:25 -0500

    correction state

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,39 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { PlayerOne } from './components/PlayerOne'
-import { PlayerTwo } from './components/PlayerTwo'
-import { InitialForm } from './components/InitialForm'
+import { FormGame } from './components/FormGame'
+import { Player } from './components/Player'
+
 
 export default function App() {
-
-  const [dataMacht, setDataMacht] = useState({
+  const [player, setPlayer] = useState({
     nameTeamOne: '',
     nameTeamTwo: '',
-    timeMacht: ''
+    hasData: false
+
+
   })
-  
+  function handleSubmit() {
+    let formGameEl = document.getElementById('form-game')
+    let dataGame = new FormData(formGameEl)
+    let nameOne = dataGame.get('name-one')
+    let nameTwo = dataGame.get('name-two')
+
+
+    setPlayer(prevPlayer => ({
+
+      ...prevPlayer,
+      hasData: true,
+      nameTeamOne: nameOne,
+      nameTeamTwo: nameTwo
+    }))
+
+  }
 
   return (
     <>
-      <InitialForm/>
+      {player.hasData === false ? <FormGame onSubmit={handleSubmit} /> : <Player nameTeamOne={player.nameTeamOne}
+        nameTeamTwo={player.nameTeamTwo} />}
     </>
   )
 }
-
-
